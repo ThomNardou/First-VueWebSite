a8042ea4bc4daa608e06b0b66fcf93ff273088d2
ReactedTitan880 <thomasnardou@icloud.com>
2024-03-12 20:24:42 +0100

    add the header

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,91 @@
 </script>
 
 <template>
-  <!-- <div class="target" v-motion-roll-visible-once-right>Hello 1</div> -->
-  <div class="target" v-motion-roll-visible-once-right>1</div>
-  <div class="target" v-motion-roll-visible-once-right>2</div>
-  <div class="target" v-motion-roll-visible-once-right>2</div>
-  <div class="target" v-motion-roll-visible-once-right>2</div>
+  <body>
+    <img src="/border.png" class="border">
+    <header>
+      <h1>Welcome in the forest WebSite</h1>
+    </header>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+    <div class="target">1</div>
+  </body>
 </template>
 
-<style scoped>
+<style>
+
+@font-face {
+  font-family: mainFont;
+  src: url(/font/GrowWestRegular-vm1qA.ttf);
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+header {
+  height: calc(100vh + 100px);
+  background-image: url("/background.jpg");
+  background-size: cover;
+  background: linear-gradient(url("/background.jpg"), 0);
+}
+
+header h1 {
+  font-family: mainFont;
+  font-size: 60px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  color: white;
+  user-select: none;
+}
+
+.border {
+  width: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  user-select: none;
+}
+
 .target {
   width: 100px;
   height: 100px;
